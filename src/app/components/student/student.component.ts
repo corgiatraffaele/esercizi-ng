8daf1445576c6821cbb82a257fa18a0de6b071df
Raf <raffele.corgiat@gmail.com>
2022-03-12 13:28:40 +0100

    esercizio ng

---
+++ src/app/components/student/student.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Student } from 'src/app/models/Student';
+
+@Component({
+  selector: 'app-student',
+  templateUrl: './student.component.html',
+  styleUrls: ['./student.component.scss']
+})
+export class StudentComponent implements OnInit {
+
+  student: Student={
+    id: 1,
+    name: 'Raffaele'
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
